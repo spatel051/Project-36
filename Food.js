5e0c702ff80eea2d9d79afd4c660492ee4c54b83
spatel051 <77207491+spatel051@users.noreply.github.com>
2021-02-13 21:42:16 -0600

    Update Food.js

--- Food.js
+++ Food.js
@@ -2,7 +2,7 @@ class Food {
     constructor() {
         this.foodStock = 11;
         this.lastFed;
-        this.image = loadImage("Milk.png");
+        this.image = loadImage("milk.png");
     }
 
     getFoodStock(){
